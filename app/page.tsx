c0ed81b900f8be23a595e51f592f2dc0163d554c
Luka <tvojemail@gmail.com>
2026-05-02 01:20:59 +0200

    fix background scale

--- app/page.tsx
+++ app/page.tsx
@@ -264,7 +264,7 @@ height: "auto",
 aspectRatio: "9 / 9",
 
 backgroundImage: `url(${getBackground()})`,
-backgroundSize: "100% 100%",
+backgroundSize: "contain",
 backgroundRepeat: "no-repeat",
 backgroundPosition: "center",
 backgroundColor: "black",
